16af70fdbf6dea1c08907321b7ffceca89fda5e8
AlidaSchefers <alidadschefers@gmail.com>
2021-04-22 18:23:54 -0400

    initial commit and solution

--- src/JavaInterviewPractice/PermMissingElem.java
+++ src/JavaInterviewPractice/PermMissingElem.java
@@ -1,3 +1,5 @@
+package JavaInterviewPractice;
+
 import java.util.Arrays;
 
 //comes from Codility
@@ -6,7 +8,8 @@ public class PermMissingElem {
     public int solution(int[] A) {
         // write your code in Java SE 8
 
-        //second solution:
+        //second solution.
+        //this gives linear run-time complexity O(n), and for memory O(1)
         int sumOfArray = 0;
         for (int i = 0; i < A.length; i++){
             sumOfArray += A[i];
@@ -17,6 +20,16 @@ public class PermMissingElem {
         }
         return sumWithoutMissingNum - sumOfArray;
         //end of second solution
+
+//        //can make the function event faster by using a "sum to x function" (x*(x+1))/2 instead of a loop:
+//        //time complexity is still O(n).
+//        int sumOfArray = 0;
+//        for (int i = 0; i < A.length; i++){
+//            sumOfArray += A[i];
+//        }
+//        int sumWithoutMissingNum = ((A.length+1 * (A.length+1+1))/2);
+//        return sumWithoutMissingNum - sumOfArray;
+
 
 //        //first solution:
 //         Arrays.sort(A);
