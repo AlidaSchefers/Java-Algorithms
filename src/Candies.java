cd795a81d7cd0397cfe14ffd5895eaa72158f3f3
AlidaSchefers <alidadschefers@gmail.com>
2021-05-05 22:09:31 -0400

    initial solution

--- src/Candies.java
+++ src/Candies.java
@@ -1,4 +1,54 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 //comes from HackerRank: https://www.hackerrank.com/challenges/candies/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
+class Result {
+
+    /*
+     * Complete the 'candies' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static long candies(int n, List<Integer> arr) {
+        // Write your code here
+        int[] distribution = new int[n];
+        distribution[0] = 1;
+        for(int i = 0; i < arr.size()-1; i++){
+            if(arr.get(i) < arr.get(i+1)){
+                distribution[i] = 1;
+                distribution[i+1] = distribution[i]+1;
+            }
+        }
+        return 9;
+    }
+
+}
 
 public class Candies {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(bufferedReader.readLine().trim());
+            arr.add(arrItem);
+        }
+
+        long result = Result.candies(n, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
