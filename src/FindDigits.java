0d80fba5a46d28f58763c7e08d11e08c600be455
AlidaSchefers <alidadschefers@gmail.com>
2021-05-09 22:39:52 -0400

    initial commit

---
+++ src/FindDigits.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.stream.IntStream;
+//comes from HackerRank: https://www.hackerrank.com/challenges/find-digits/problem
+
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        // Write your code here
+
+    }
+
+}
+
+public class FindDigits {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
