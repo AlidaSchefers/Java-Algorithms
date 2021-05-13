9736f496dd3844c5c70bedbbe2b9d05fac6b1641
AlidaSchefers <alidadschefers@gmail.com>
2021-05-12 22:28:58 -0400

    initial solution

--- src/BearandSteadyGene.java
+++ src/BearandSteadyGene.java
@@ -11,8 +11,55 @@ class Result {
      * The function accepts STRING gene as parameter.
      */
 
+    public static boolean isAllZero(HashMap<Character, Integer> hm){
+        for (Integer i : hm.values()) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static int steadyGene(String gene) {
         // Write your code here
+        HashMap<Character, Integer> letterInstances = new HashMap<Character, Integer>();
+        letterInstances.put('A', 0);
+        letterInstances.put('C', 0);
+        letterInstances.put('T', 0);
+        letterInstances.put('G', 0);
+        for(int i = 0; i < gene.length(); i++){
+            letterInstances.replace(gene.charAt(i), letterInstances.get(gene.charAt(i))+1);
+        }
+        // System.out.println(letterInstances);
+        HashMap<Character, Integer> overRepLetters = new HashMap<Character, Integer>();
+        letterInstances.forEach((k, v) -> {
+            if(v > (gene.length()/4)){
+                overRepLetters.put(k, (v - (gene.length()/4)));
+            }
+        });
+        int smallestSubString = gene.length();
+        for(int i = 0; i < gene.length(); i++){
+            if(overRepLetters.containsKey(gene.charAt(i))){
+                HashMap<Character, Integer> tempOverRepLetters = overRepLetters;
+                int index = i;
+                int substringLength = 0;
+                // while(!isAllZero(tempOverRepLetters)){
+                //     if(overRepLetters.containsKey(gene.charAt(index))){
+                //         overRepLetters.replace(gene.charAt(index), overRepLetters.get(gene.charAt(index))-1);
+                //         substringLength++;
+                //         index++;
+                //     }else{
+                //         substringLength++;
+                //         index++;
+                //     }
+                // }
+                // if(substringLength < smallestSubString) {
+                //     smallestSubString = substringLength;
+                // }
+            }
+        }
+
+        return 5; //temporary
     }
 
 }
