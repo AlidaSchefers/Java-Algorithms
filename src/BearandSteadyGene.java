4ff616b0cb354c530b022865f75b18a28e2107ee
AlidaSchefers <alidadschefers@gmail.com>
2021-05-12 22:28:25 -0400

    initial commit

---
+++ src/BearandSteadyGene.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'steadyGene' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING gene as parameter.
+     */
+
+    public static int steadyGene(String gene) {
+        // Write your code here
+    }
+
+}
+
+public class BearandSteadyGene {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String gene = bufferedReader.readLine();
+
+        int result = Result.steadyGene(gene);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
