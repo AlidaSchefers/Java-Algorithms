821bf77906a8f3a5a7433df92ffe93978f29635f
AlidaSchefers <alidadschefers@gmail.com>
2021-05-17 23:02:24 -0400

    initial solution

--- src/IsThisABST.java
+++ src/IsThisABST.java
@@ -10,8 +10,30 @@ The Node class is defined as follows:
     Node right;
      }
 */
+    boolean checkNode(Node root, int upperLimit, int lowerLimit){
+        if(root.left != null){
+            if(root.data < upperLimit) {upperLimit = root.data;}
+            if(root.left.data > lowerLimit && root.left.data < upperLimit) {
+                checkNode(root.left, upperLimit, lowerLimit);
+            }else{
+                return false;
+            }
+        }
+        if(root.right != null){
+            if(root.data > lowerLimit) {lowerLimit = root.data;}
+            if(root.right.data > lowerLimit && root.right.data < upperLimit){
+                checkNode(root.right, upperLimit, lowerLimit);
+            }
+        }
+        if(root.left == null && root.right == null){
+            return false;
+        }
+        return true; //temporary to avoid automatic error
+    }
     boolean checkBST(Node root) {
-
+        int upperLimit = root.data;
+        int lowerLimit = 0;
+        return checkNode(root, upperLimit, lowerLimit);
     }
 }
 
