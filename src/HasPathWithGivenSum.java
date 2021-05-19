565bc4f768f0edbc217b285494218eaaedd30ab4
AlidaSchefers <alidadschefers@gmail.com>
2021-05-18 22:31:02 -0400

    initial commit

---
+++ src/HasPathWithGivenSum.java
@@ -0,0 +1,15 @@
+public class HasPathWithGivenSum {
+}
+// Binary trees are already defined with this interface:
+//class Tree<T> {
+//    Tree(T x) {
+//        value = x;
+//    }
+//    T value;
+//    Tree<T> left;
+//    Tree<T> right;
+//}
+    boolean hasPathWithGivenSum(Tree<Integer> t, int s) {
+
+
+    }
