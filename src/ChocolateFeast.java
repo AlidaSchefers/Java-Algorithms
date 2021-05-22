a300039ef3f0dad89c9c1cad6fdc192492afdcec
AlidaSchefers <alidadschefers@gmail.com>
2021-05-21 23:02:30 -0400

    initial commit

---
+++ src/ChocolateFeast.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.stream.*;
+
+//come from HackerRank: https://www.hackerrank.com/challenges/chocolate-feast/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+class Result {
+
+    /*
+     * Complete the 'chocolateFeast' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER c
+     *  3. INTEGER m
+     */
+
+    public static int chocolateFeast(int n, int c, int m) {
+        // Write your code here
+
+    }
+
+}
+
+public class ChocolateFeast {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int c = Integer.parseInt(firstMultipleInput[1]);
+
+                int m = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.chocolateFeast(n, c, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
