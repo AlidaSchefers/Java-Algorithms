9f9c07f448e15e9d404d94edee47ca3f44e58f38
AlidaSchefers <alidadschefers@gmail.com>
2021-05-24 22:34:22 -0400

    initial commit

---
+++ src/IsListPalindrome.java
@@ -0,0 +1,14 @@
+//comes from CodeSignal: https://app.codesignal.com/interview-practice/task/HmNvEkfFShPhREMn4/description
+
+// Singly-linked lists are already defined with this interface:
+// class ListNode<T> {
+//   ListNode(T x) {
+//     value = x;
+//   }
+//   T value;
+//   ListNode<T> next;
+// }
+//
+boolean IsListPalindrome(ListNode<Integer> l) {
+
+}
