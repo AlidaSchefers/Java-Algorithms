b4d7a6f726ed28975e9add3b1257577233a761c4
AlidaSchefers <alidadschefers@gmail.com>
2021-05-26 21:41:11 -0400

    transform to ArrayList solution

--- src/IsListPalindrome.java
+++ src/IsListPalindrome.java
@@ -9,6 +9,32 @@
 //   ListNode<T> next;
 // }
 //
-boolean IsListPalindrome(ListNode<Integer> l) {
 
+//------------------------------------------------- transforming to an ArrayList
+boolean isArrListPalindrome(ArrayList<Long> arr){
+    int halfwayMark = arr.size() % 2 == 0 ? (arr.size()/2) : (arr.size()/2)-1;
+    int idx = 0;
+    while(idx < halfwayMark){
+        if(!Objects.equals(arr.get(halfwayMark-1-idx), arr.get(halfwayMark+idx))){
+            return false;
+        }else{
+            idx++;
+        }
+    }
+    return true;
+}
+
+boolean isListPalindrome(ListNode<Integer> l) {
+    //iterate through l to make an arrayList version
+    //make and use a new method to test if arrayList is Palindrome
+    //whether arrayList is odd or even length, calculate how far in you need to look with rounding down dision.
+    //iterate through arrayList and compare two oppositely placed elements. Start inward and move outwards
+    //if the two elements are ever not the same, return false.
+    //if the loop ends (all are the same), return true.
+    ArrayList<Long> lArr = new ArrayList<>();
+    while(l!=null){
+        lArr.add((long)l.value);
+        l = l.next;
+    }
+    return isArrListPalindrome(lArr);
 }
