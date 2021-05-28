3e008972baf51055e9768b1c6cdf26a3de9ecadb
AlidaSchefers <alidadschefers@gmail.com>
2021-05-27 22:06:13 -0400

    initial commit

---
+++ src/AbsolutePermutation.java
@@ -0,0 +1,58 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+
+
+class Result {
+
+    /*
+     * Complete the 'absolutePermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     */
+
+
+    public static List<Integer> absolutePermutation(int n, int k) {
+        // Write your code here
+
+    }
+
+}
+
+public class AbsolutePermutation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int k = Integer.parseInt(firstMultipleInput[1]);
+
+                List<Integer> result = Result.absolutePermutation(n, k);
+
+                bufferedWriter.write(
+                        result.stream()
+                                .map(Object::toString)
+                                .collect(joining(" "))
+                                + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
