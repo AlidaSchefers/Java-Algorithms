658971bacf2ea0894749c44a0adc82f2c52573be
AlidaSchefers <alidadschefers@gmail.com>
2021-05-28 15:33:45 -0400

    solutions that pass 9/11 test cases

--- src/AbsolutePermutation.java
+++ src/AbsolutePermutation.java
@@ -4,43 +4,87 @@ import java.util.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 
+
 //comes from HackerRank: https://www.hackerrank.com/challenges/absolute-permutation/problem?isFullScreen=true
+
+//the two solutions solve 9/11 test cases and exceeds the time limit for the last two test cases
+
+//creates the list from first value to last.
+//class Result {
+//     public static List<Integer> absolutePermutation(int n, int k) {
+//     //3 conditions/rules
+//         //1. are both num in the range? 1-n
+//         //yes: take lower num
+//         //2. always take lower num
+//         //3. no repeats.
+//         //1. no repeats, 2. num is in range, 3. take lower num unless break 1 or 2, 4. if no num available, return list of -1.
+//         List<Integer> PermutationList = new ArrayList<Integer>();
+//         int[] availability = new int[n];
+//         for(int i = 0; i < n; i++){
+//             availability[i] = 1;
+//         }
+//         int i = 1;
+//         while(i <= n){
+//             int option1 = i+k;
+//             int option2 = i-k;
+//             //decide which option to add to PermutationList or return list with just -1.
+//             boolean option1IsValid = (option1 <= n && option1 > 0 && availability[option1-1] == 1) ? true : false;
+//             boolean option2IsValid = (option2 <= n && option2 > 0 && availability[option2-1] == 1) ? true : false;
+//             int valToAdd;
+//             if(option1IsValid && option2IsValid){
+//                 valToAdd = (option1 < option2) ? option1 : option2;
+//                 System.out.println("valToAdd: "+valToAdd);
+//                 PermutationList.add(valToAdd);
+//                 availability[valToAdd-1]--;
+//                 i++;
+//             }else if(!option1IsValid && !option2IsValid){
+//                 return new ArrayList<Integer>(Arrays.asList(-1));
+//             }else{ //only one of the options is valid
+//                 valToAdd =  (option1IsValid) ? option1 : option2;
+//                 PermutationList.add(valToAdd);
+//                 availability[valToAdd-1]--;
+//                 i++;
+//             }
+
+//         }
+//         //after while loop
+//         return PermutationList;
+//     }
+
+// }
+
+//creates list from last element to first.
 class Result {
-
-    /*
-     * Complete the 'absolutePermutation' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER k
-     */
-
-
     public static List<Integer> absolutePermutation(int n, int k) {
-        // Write your code here
-        //3 conditions/rules
-        //1. are both num in the range? 1-n
-        //yes: take lower num
-        //2. always take lower num
-        //3. no repeats.
-
-        //1. no repeats, 2. num is in range, 3. take lower num unless break 1 or 2, 4. if no num available, return list of -1.
         List<Integer> PermutationList = new ArrayList<Integer>();
-        int[] freqs = int[n];
+        int[] availability = new int[n];
         for(int i = 0; i < n; i++){
-            int[i] = 1;
+            availability[i] = 1;
         }
-        int i = 1;
-        while(i <= n){
+        int i = n;
+        while(i > 0){
             int option1 = i+k;
             int option2 = i-k;
             //decide which option to add to PermutationList or return list with just -1.
-            i++;
+            boolean option1IsValid = (option1 <= n && option1 > 0 && availability[option1-1] == 1) ? true : false;
+            boolean option2IsValid = (option2 <= n && option2 > 0 && availability[option2-1] == 1) ? true : false;
+            int valToAdd;
+            if(option1IsValid && option2IsValid){
+                valToAdd = (option1 > option2) ? option1 : option2;
+                PermutationList.add(0, valToAdd);
+                availability[valToAdd-1]--;
+                i--;
+            }else if(!option1IsValid && !option2IsValid){
+                return new ArrayList<Integer>(Arrays.asList(-1));
+            }else{ //only one of the options is valid
+                valToAdd = (option1IsValid) ? option1 : option2;
+                PermutationList.add(0, valToAdd);
+                availability[valToAdd-1]--;
+                i--;
+            }
+
         }
-
-
-
+        //after while loop
         return PermutationList;
     }
 
