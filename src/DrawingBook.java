9abc4864b67650c27cf647bf4a34be1322851acb
AlidaSchefers <alidadschefers@gmail.com>
2021-05-30 22:42:12 -0400

    initial commit

---
+++ src/DrawingBook.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+//comes from HackerRank: https://www.hackerrank.com/challenges/drawing-book/problem
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+
+    }
+
+}
+
+public class DrawingBook {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
