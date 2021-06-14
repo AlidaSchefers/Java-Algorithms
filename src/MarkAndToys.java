adee9687d14cf8ce8580c800c0fd9559acdbca11
AlidaSchefers <alidadschefers@gmail.com>
2021-06-13 22:39:39 -0400

    initial commit and complete solution

---
+++ src/MarkAndToys.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+//comes from HackerRank: https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+class Result {
+    public static int maximumToys(List<Integer> prices, int k) {
+        Collections.sort(prices);
+        int toyTotal = 0;
+        for(int price : prices){
+            if(price < k){
+                toyTotal++;
+                k -= price;
+            }else{
+                return toyTotal;
+            }
+        }
+        return toyTotal;
+
+    }
+
+}
+
+public class MarkAndToys {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
