7910256d059d29063399ae07cd6c32fafccf8995
AlidaSchefers <alidadschefers@gmail.com>
2021-06-15 22:53:15 -0400

    solution pseudocode

--- src/CountingValleys.java
+++ src/CountingValleys.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 
+//comes from HackerRank: https://www.hackerrank.com/challenges/counting-valleys/problem
 class Result {
 
     /*
@@ -13,7 +14,28 @@ class Result {
      */
 
     public static int countingValleys(int steps, String path) {
+        //problem notes:
+        //Hikes always start and end at sea level
+        //mountain: sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+        //valley: sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+        //find and print the number of valleys walked through.
 
+        //solution notes:
+        //initialize altitude to 0;
+        //bool isInValley = false;
+        //bool isInMountain = false;
+        //int valleyCount = 0;
+        //loop through string characters. if U, increase altitude by 1. if D, descrease altitude by 1.
+            //with each loop,
+                //if altitude positive: isInMountain = true;
+                //if altitude negative: isInValley = true;
+                //if altitude is 0
+                    //if isInValley is true:
+                        //valleyCount++
+                        //isInValley = false;
+                    //if isInMountain is true:
+                        //isInMountain = false;
+        //return valleyCount;
     }
 
 }
