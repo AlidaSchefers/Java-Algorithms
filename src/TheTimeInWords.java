ea8b5e0a10b9b59404003908fe6492e8f750f1cf
AlidaSchefers <alidadschefers@gmail.com>
2021-06-17 21:40:29 -0400

    initial commit

---
+++ src/TheTimeInWords.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+//comes from HackerRank: https://www.hackerrank.com/challenges/the-time-in-words/problem
+class Result {
+
+    /*
+     * Complete the 'timeInWords' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER h
+     *  2. INTEGER m
+     */
+
+    public static String timeInWords(int h, int m) {
+
+    }
+
+}
+
+public class TheTimeInWords {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int h = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.timeInWords(h, m);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
