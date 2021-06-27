73768b82fbccf8204d83599fa1c51f95b1efb37b
AlidaSchefers <alidadschefers@gmail.com>
2021-06-26 22:32:43 -0400

    complete solution

--- src/JumpingOnTheClouds.java
+++ src/JumpingOnTheClouds.java
@@ -6,17 +6,28 @@ import static java.util.stream.Collectors.toList;
 //comes from HackerRank: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
 class Result {
 
-    /*
-     * Complete the 'jumpingOnClouds' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY c as parameter.
-     */
-
     public static int jumpingOnClouds(List<Integer> c) {
-
+        //start with index 0, so initialize i to 0.
+        int i = 0;
+        //initialize jumps to 0.
+        int jumps = 0;
+        //keep loop going until the index i becomes that of the last element (top cloud)
+        while(i < c.size()-1){
+            //if i is equal to c.size()-2 (index of second to last cloud)
+            if(i == c.size()-2){
+                //increase jumps count by 1
+                jumps++;
+                //break
+                break;
+            }else{
+                //increase jumps count by 1
+                jumps++;
+                //new value of i is i+2 if value at index i+2 is 0. else, it is i+1.
+                i = (c.get(i+2) == 0) ? i+2 : i+1;
+            }
+        }
+        return jumps;
     }
-
 }
 
 public class JumpingOnTheClouds {
