f73bb3c8dfdf2ca2b637d5a1c9a1f67ca8b75ce0
AlidaSchefers <alidadschefers@gmail.com>
2021-07-01 22:56:49 -0400

    initial commit

---
+++ src/apple/AssortedSquares.java
@@ -0,0 +1,31 @@
+package apple;
+
+import java.util.ArrayList;
+
+public class AssortedSquares(int[] arr) {
+    int[] arrEx = new int[]{-7, -3, -1, 0, 2, 5, 8, 15};
+    //set i to first element's index
+    int i = 0;
+    //set j to the last element's index
+    int j = 0;
+    //initalize empty array newArr
+    while(i != j){
+        if(Math.pow(arr[i], 2) > Math.pow(arr[j], 2)){
+            //add Math.pow(arr[i], 2) to the front of newArr
+            //i++
+        }else{
+            //add Math.pow(arr[j], 2) to the front of newArr
+            //j++
+        }
+    }
+    //add Math.pow(arr[i], 2) to the front of newArr
+    //return newArr
+
+
+
+
+
+    //for each element in the array
+        //square the element
+
+}
