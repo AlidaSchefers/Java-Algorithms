e7020c9aa20ee1ab3f1899fcd483a681237c9afe
AlidaSchefers <alidadschefers@gmail.com>
2021-07-11 16:35:54 -0400

    solution

--- src/LeftRotation.java
+++ src/LeftRotation.java
@@ -9,10 +9,23 @@ class Result {
 
     public static List<Integer> rotLeft(List<Integer> arr, int rotations) {
         //if rotations is 0, return the array as is.
+        if(rotations == 0){
+            return arr;
+        }
         //get remainder of rotations divided by the array length (using % modulo)
+        int rotRemainder = rotations % arr.size();
         //if the remainder is 0, return the array as is.
-        //else: add the remainder number of first elements to the end of the array, then delete that remainder number of first elements in the array.
+        if(rotRemainder == 0){
+            return arr;
+        }else{
+            //else: add the remainder number of first elements to the end of the array, then delete that remainder number of first elements in the array.
+            for(int i = 1; i <= rotRemainder; i++){
+                arr.add(arr.get(0));
+                arr.remove(0);
+            }
+        }
         //return the array
+        return arr;
     }
 
 }
