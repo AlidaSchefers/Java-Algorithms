9e0e14aee6f3a2bd0486656ba2d3bf95b086b2e3
AlidaSchefers <alidadschefers@gmail.com>
2021-07-16 22:47:01 -0400

    initial commit and initial comments

---
+++ src/SherlockAndCost.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+//comes from HackerRank: https://www.hackerrank.com/challenges/sherlock-and-cost/problem
+class Result {
+
+    public static int cost(List<Integer> B) {
+        //array A with biggest sum of pair differences should have elements that are either 1 or B[i]
+        //the key is to determine which A[i] elements are B[i] or 1.
+    }
+
+}
+
+public class SherlockAndCost {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> B = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                int result = Result.cost(B);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
