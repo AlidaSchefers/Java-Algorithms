4798936432a7788f768e0b92435a67e2e5b579af
AlidaSchefers <alidadschefers@gmail.com>
2021-07-21 23:06:58 -0400

    initial commit and comments

---
+++ src/IceCreamParlor.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> icecreamParlor(int pocketMoney, List<Integer> prices) {
+        // Write your code here
+
+        //initialize empty toBuy ArrayList
+        //iterate through prices arr
+        //if current index's value is >= pocketMoney, skip
+        //else, do another loop where you add current index's value and value of the other indexes, and if add up to pocketMoney, return those indexes.
+
+    }
+
+}
+
+public class IceCreamParlor {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                List<Integer> result = Result.icecreamParlor(m, arr);
+
+                bufferedWriter.write(
+                        result.stream()
+                                .map(Object::toString)
+                                .collect(joining(" "))
+                                + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
