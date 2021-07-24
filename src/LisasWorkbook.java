52ac7188d1c495d8cf7945e4928b7032330586aa
AlidaSchefers <alidadschefers@gmail.com>
2021-07-23 22:56:41 -0400

    initial commit

---
+++ src/LisasWorkbook.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+//comes from HackerRank: https://www.hackerrank.com/challenges/lisa-workbook/problem
+class Result {
+
+    public static int workbook(int chapters, int problemsPerPage, List<Integer> problemsPerChapterArr) {
+
+    }
+
+}
+
+public class LisasWorkbook {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.workbook(n, k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
